01827d21faf8ef693e47a37f01619039bc4d67e3
arslaan-twi <akhalid@thewaterinstitute.org>
2026-08-26 18:06:42 -0400

    Update app.py

--- lwi-aep-comparison-dashboard/app.py
+++ lwi-aep-comparison-dashboard/app.py
@@ -337,8 +337,7 @@ st.markdown(
     """
     <div style="text-align: center; color: gray; font-size: 0.85em;">
     <b>DRAFT VERSION — NOT FOR REDISTRIBUTION</b><br>
-    Not to be used for decision making.<br>
-    © The Water Institute
+    Not to be used for decision making. © The Water Institute
     </div>
     """,
     unsafe_allow_html=True,
